493bce63badf7c76ca99c37fb3da51b2f6b384f3
raifulhaque953-cmd <raifulhaque953@gmail.com>
2025-09-28 10:48:10 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,35 @@
+from wsgi import app
+
+#,                         ︵
+#                        /'_/) 
+#                      /¯ ../ 
+#                    /'..../ 
+#                  /¯ ../ 
+#                /... ./
+#   ¸•´¯/´¯ /' ...'/´¯`•¸  
+# /'.../... /.... /.... /¯\
+#('  (...´.(,.. ..(...../',    \
+# \'.............. .......\'.    )      
+#   \'....................._.•´/
+#     \ ....................  /
+#       \ .................. |
+#         \  ............... |
+#           \............... |
+#             \ .............|
+#               \............|
+#                 \ .........|
+#                   \ .......|
+#                     \ .....|
+#                       \ ...|
+#                         \ .|
+#                           \\
+#                             \('-') 
+#   ,,                           |_|\
+#                               | |
+#FUCKED BY JOBAYAR AHMED @JOBAYAR_AHMED 
+#FUCKED BY JOBAYAR AHMED @JOBAYAR_AHMED 
+#FUCKED BY JOBAYAR AHMED @JOBAYAR_AHMED 
+#FUCKED BY JOBAYAR AHMED @JOBAYAR_AHMED 
+#FUCKED BY JOBAYAR AHMED @JOBAYAR_AHMED 
+#FUCKED BY JOBAYAR AHMED @JOBAYAR_AHMED 
+#FUCKED BY JOBAYAR AHMED @JOBAYAR_AHMED 
